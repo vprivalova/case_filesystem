8aa8666c00cf814db10ea8f37c90a6c878ebf2dd
Anastasia030 <a.nizovtseva@g.nsu.ru>
2023-12-04 00:52:32 +0700

    move down

--- move_down.py
+++ move_down.py
@@ -1,6 +1,13 @@
+import os
+
+
 def moveDown(currentDir):
-    list = os.listdir(path=".")
-    while currentDir not in
+    path = os.getcwd()
+    catalogs = os.listdir(path=".")
+    while currentDir not in catalogs:
+        currentDir = input('Пожалуйста, повторите попытку ввода имени подкаталога: ')
+    new_catalog = path + "\\" + currentDir
+    return os.chdir(new_catalog)
 
 
 if __name__ == '__main__':
