b076e1871a5894a98b0476181c7cb86dcc999d61
vprivalova <privalovaa.vv@gmail.com>
2023-12-03 23:37:39 +0700

    commit2

--- acceptCommand.py
+++ acceptCommand.py
@@ -1,9 +1,10 @@
-def acceptCommand():
+def aссeptСommand():
     numbers_point = ["1", "2", "3", "4", "5", "6", "7"]
     command = input('Выберите пункт меню: ')
     while command not in numbers_point:
         command = input('Пожалуйста, повторите попытку ввода номера команды')
+    return command
 
 
 if __name__ == '__main__':
-    acceptCommand()
+    aссeptСommand()
--- filesystem.py
+++ filesystem.py
@@ -1,12 +1,13 @@
 import os
 import acceptCommand
+import runCommand
 
 MENU = '''
     Меню:
     1. Просмотр каталога
     2. На уровень вверх
     3. На уровень вниз
-    4. Количество файлов и катаогов
+    4. Количество файлов и каталогов
     5. Размер текущего каталога (в байтах)
     6. Поиск файла
     7. Выход из программы
@@ -17,8 +18,8 @@ def main():
     while True:
         print(os.getcwd())
         print(MENU)
-        command = acceptCommand()
-        runCommand(command)
+        command = acceptCommand.acceptCommand()
+        runCommand.runCommand(command)
         if command == '7':
             print('Работа программы завершена.')
             break
